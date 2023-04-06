a7f086024ae215ee0851fced46225bca725e2b37
Mariane Mendes <mariane-cristina@uol.com.br>
2023-04-06 03:28:54 -0700

    teste

--- src/FrontEnd/index.js
+++ src/FrontEnd/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './frontend/App.js';
+import App from './App.js';
 
 const rootElement = document.getElementById('App');
 const root = createRoot(rootElement);
