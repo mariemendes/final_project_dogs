7db595a14f8aa3ab334ad515b7cb9b3d4ac4e73f
Mariane Mendes <mariane-cristina@uol.com.br>
2023-04-06 03:39:56 -0700

    test2

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './index.js',
+  entry: './frontend/index.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
